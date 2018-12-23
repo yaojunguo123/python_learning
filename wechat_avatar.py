f82f82526d23d1086a75f413929b2f83a9556f7e
zero_glo <13269034777@163.com>
2018-12-23 10:26:05 +0800

    生成微信朋友圈图片

--- wechat_avatar.py
+++ wechat_avatar.py
@@ -6,28 +6,28 @@ from PIL import Image
 import os
 
 
-itchat.auto_login()
-friends = itchat.get_friends(update=True)
+itchat.auto_login()#微信扫码登录
+friends = itchat.get_friends(update=True)#获取好友列表
 num = 0
 if not os.path.exists("image"):
 	os.mkdir("image")
 for friend in friends:
-	img = itchat.get_head_img(userName = friend['UserName'])
-	
+	img = itchat.get_head_img(userName = friend['UserName'])#获取好友头像图片
+	# 循环生成图片保存到 image 文件夹
 	fileImage = open('image'+'/'+str(num)+'.jpg','wb')
 	fileImage.write(img)
 	fileImage.close()
 	num += 1
 all_image = os.listdir('image')
-each_size = int(math.sqrt(float(640*640)/len(all_image)))
+each_size = int(math.sqrt(float(640*640)/len(all_image))) #每个图片在最后图片中的大小
 print(each_size)
-lines = int(640/each_size)
+lines = int(640/each_size) #图片的边长
 image = Image.new('RGB',(640,640))
 x = 0
 y = 0
 for i in range(0,len(all_image)):
 	try:
-		img = Image.open('image'+"/"+str(i)+".jpg")
+		img = Image.open('image'+"/"+str(i)+".jpg")#打开之前获得的图片  该表尺寸 放到 img里面
 		img = img.resize((each_size,each_size),Image.ANTIALIAS)
 		image.paste(img,(x*each_size,y*each_size))
 		x += 1
@@ -36,7 +36,7 @@ for i in range(0,len(all_image)):
 			y += 1
 	except:
 		pass
-image.save('image'+"/"+"all.jpg")
-itchat.send_image('image'+"/"+"all.jpg",'filehelper')
+image.save('image'+"/"+"all.jpg")#baocun
+itchat.send_image('image'+"/"+"all.jpg",'filehelper')  # 发送到文件传输助手
 
 
